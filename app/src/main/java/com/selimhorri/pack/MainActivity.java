56a86a4e90057318654d50aebe8d1abd6fc32a1b
Selim Horri <horri.slim@gmail.com>
2021-05-16 02:33:45 +0100

    add volley dependency and user internet permission

--- app/src/main/java/com/selimhorri/pack/MainActivity.java
+++ app/src/main/java/com/selimhorri/pack/MainActivity.java
@@ -3,13 +3,26 @@ package com.selimhorri.pack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private TextView textView;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = super.findViewById(R.id.textViewDisplayer);
+        button = super.findViewById(R.id.btnApiCall);
+
+
+
     }
-    
+
+
+
 }
