30d5640d6d200871483206e75e67779a1f41cfdf
Selim Horri <horri.slim@gmail.com>
2021-05-16 03:14:38 +0100

    make an api call successfully

--- app/src/main/java/com/selimhorri/pack/MainActivity.java
+++ app/src/main/java/com/selimhorri/pack/MainActivity.java
@@ -1,10 +1,20 @@
 package com.selimhorri.pack;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +26,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = super.findViewById(R.id.textViewDisplayer);
-        button = super.findViewById(R.id.btnApiCall);
+        this.textView = super.findViewById(R.id.textViewDisplayer);
+        this.button = super.findViewById(R.id.btnApiCall);
 
+        final String url = "https://project-tracking-system-heroku.herokuapp.com/app/api/userCredentials";
+        this.button.setOnClickListener(v -> {
 
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                (response) -> this.textView.setText(response.toString()),
+                (error) -> this.textView.setText("did not workkkk")
+            );
+
+            requestQueue.add(jsonObjectRequest);
+
+        });
 
     }
 
