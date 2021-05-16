f8dd45c70a3c33769a4a2428aee0ab1a9ef08625
Selim Horri <horri.slim@gmail.com>
2021-05-16 02:26:34 +0100

    modify layout

--- app/src/main/java/com/selimhorri/pack/MainActivity.java
+++ app/src/main/java/com/selimhorri/pack/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
 }
